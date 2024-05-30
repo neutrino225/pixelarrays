f533ad3bebd2a08e785248c09bb736cd226de7bf
neutrino225 <fznkrmt22503@gmail.com>
2024-05-31 02:48:48 +0500

    SpeedInsights Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
 	weight: ["200", "400", "700"], // Specify the weights you need
@@ -24,6 +25,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={poppins.className}>
 				{children}
+				<SpeedInsights />
 				<Footer />
 			</body>
 		</html>
